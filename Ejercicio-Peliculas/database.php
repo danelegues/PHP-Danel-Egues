79d25cf6fa479fcd0306a52740d68ad7f9c6a75c
ubuntu <deguesju23dw@ikzubirimanteo.com>
2024-11-06 08:30:53 +0100

    Añadir tabla de peliculas

--- Ejercicio-Peliculas/database.php
+++ Ejercicio-Peliculas/database.php
@@ -14,10 +14,27 @@
                     nombreUser VARCHAR(100) NOT NULL PRIMARY KEY,
                     passwdUser VARCHAR(255) NOT NULL)";
 
-    if ($conn -> query($tablaUsuario) === TRUE) {
-        echo "Tabla creada exitosamente";
+    if ($conn->query($tablaUsuario) === TRUE) {
+        echo "Tabla Usuarios creada exitosamente<br>";
     } else {
-        echo "Error creando la tabla " . $conn->error;
+        echo "Error creando la tabla Usuarios: " . $conn->error . "<br>";
+    }
+
+    $tablaPeliculas = "CREATE TABLE IF NOT EXISTS Peliculas (
+                    usuario VARCHAR(100) NULL,
+                    ISAN INT(8),
+                    nombrePelícula VARCHAR(100) NOT NULL PRIMARY KEY,
+                    puntuacionPelícula INT(11),
+                    año INT(11),
+                    INDEX(usuario),
+                    FOREIGN KEY (usuario) REFERENCES Usuarios(nombreUser)
+                    ON DELETE RESTRICT ON UPDATE RESTRICT)";
+
+    if ($conn->query($tablaPeliculas) === TRUE) {
+        echo "Tabla Peliculas creada exitosamente";
+    } else {
+        echo "Error creando la tabla Peliculas: " . $conn->error;
     }
 
     $conn->close();
+?>
